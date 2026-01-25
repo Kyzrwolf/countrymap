0354cb0aec6ceef5339679a423d26e41202131cc
kyzrwolf <kaizerzavr@gmail.com>
2026-01-25 10:39:01 +0300

    main: Удалил настройку Flyway и связанные с ним зависимости, упростил конфигурацию приложения. Исправил эндпоинты в CountryController. Обновил версию Spring Boot в build.gradle.kts.

--- src/main/java/io/student/countrymap/CountrymapApplication.java
+++ src/main/java/io/student/countrymap/CountrymapApplication.java
@@ -1,24 +1,13 @@
 package io.student.countrymap;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CountrymapApplication implements CommandLineRunner {
-
-    @Autowired(required = false)
-    private Flyway flyway;
+public class CountrymapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CountrymapApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        System.out.println("Flyway bean = " + flyway);
     }
 }
 
